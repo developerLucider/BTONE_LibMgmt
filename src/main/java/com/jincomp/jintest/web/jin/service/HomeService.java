b55ebb9fcd59fd90ceb43a2b2f34f03fc4f98ea0
'mskim' <mskim@btone.co.kr>
2023-03-22 13:49:24 +0900

    1.공용헤더 조건 변경 2. 로그인유저만 대여하도록 수정 및 대여 시 회원 적용

--- src/main/java/com/jincomp/jintest/web/jin/service/HomeService.java
+++ src/main/java/com/jincomp/jintest/web/jin/service/HomeService.java
@@ -26,68 +26,11 @@ import lombok.RequiredArgsConstructor;
 public class HomeService {
 
 	private static final Logger logger = LoggerFactory.getLogger(HomeService.class);
-
 	private final BookMapper bookMapper;
-
-	public List<MainBookListDTO> getMainBookList() {
-		logger.debug("-------------------userService bookList 진입");
+	
+	private List<MainBookListDTO> addEventPrice(List<BookVO> bookList) {
 		List<MainBookListDTO> mainList = new ArrayList<>();
 		
-		List<BookVO> bookList = bookMapper.getBookList();
-		for(BookVO tmp : bookList) {
-			// 가져온 리스트에서 책별로 필요한 정보만 가져와 DTO에담아서 리스트로 생성
-			MainBookListDTO mainbook = new MainBookListDTO();
-			
-			mainbook.setGoodsId(tmp.getGoodsId());
-			mainbook.setGoodsName(tmp.getGoodsName());
-			mainbook.setGoodsPrice(tmp.getGoodsPrice());
-			if(!Objects.isNull(tmp.getEventId())) {
-				mainbook.setEventStrDate(tmp.getEventVo().getRateStrDay());
-				mainbook.setEventEndDate(tmp.getEventVo().getRateEndDay());
-			}
-			mainbook.setUserId(tmp.getRentVo().getUserNo());
-			
-			// 이벤트 유무에 따라 할인가를 계산해준다.
-			logger.debug("{}의 이벤트 : {}", tmp.getGoodsName() , tmp.getEventId());
-			logger.debug("해당 책의 이벤트시작일 : {} 이벤트 종료일 : {}",
-					tmp.getEventVo().getRateStrDay(), tmp.getEventVo().getRateEndDay());
-	
-			
-			if(Objects.isNull(tmp.getEventId())) {	// 해당 이벤트가 없으면 할인가에 정가를 삽입
-				mainbook.setGoodsDiscountPrice(tmp.getGoodsPrice());
-			} else {
-				//logger.debug("이벤트 고정할인가 = {}\n 이벤트 할인율 = {}",tmp.getEventVo().getFixDiscount(),tmp.getEventVo().getFixDiscount());
-				int bookPrice = Integer.parseInt(tmp.getGoodsPrice());	// string 타입인 원가를 int형으로 변환 (계산을 위해)
-				
-				int rateTmp = tmp.getEventVo().getRate();
-				
-				double rateDouble = rateTmp;
-				
-				if(tmp.getEventVo().getFixDiscount() != 0) {	// 고정 할인가가 존재하는 경우
-					bookPrice -= tmp.getEventVo().getFixDiscount();	 // 고정 할인가 만큼 감소
-					mainbook.setGoodsDiscountPrice(String.valueOf(bookPrice));
-				}
-				
-				if(tmp.getEventVo().getRate() != 0) {
-					
-					double dcPrice = bookPrice * (rateDouble / (double)100);
-					logger.debug("할인비율가 = {}", dcPrice);
-					bookPrice -= dcPrice;
-					mainbook.setGoodsDiscountPrice(String.valueOf(bookPrice));
-				}
-			}
-			mainList.add(mainbook);
-		}
-		logger.debug("booklist : {}", mainList);
-		
-		return mainList;
-	}
-	
-	public List<MainBookListDTO> getMainBookList(String searchType, String keyword) {
-		logger.debug("-------------------userService bookList 진입");
-		List<MainBookListDTO> mainList = new ArrayList<>();
-		
-		List<BookVO> bookList = bookMapper.searchBookList(searchType, keyword);
 		for(BookVO tmp : bookList) {
 			// 가져온 리스트에서 책별로 필요한 정보만 가져와 DTO에담아서 리스트로 생성
 			MainBookListDTO mainbook = new MainBookListDTO();
@@ -129,7 +72,23 @@ public class HomeService {
 		}
 		logger.debug("booklist : {}", mainList);
 		
-		return mainList;
+		return mainList; 
+	}
+	
+	public List<MainBookListDTO> getMainBookList() {
+		logger.debug("-------------------userService bookList 진입");
+		
+		List<BookVO> bookList = bookMapper.getBookList();
+		
+		return addEventPrice(bookList);
+	}
+	
+	public List<MainBookListDTO> getMainBookList(String searchType, String keyword) {
+		logger.debug("-------------------userService bookList 진입");
+
+		List<BookVO> bookList = bookMapper.searchBookList(searchType, keyword);
+		
+		return addEventPrice(bookList);
 	}
 	
 	public List<Integer> rentBooks(List<String> rentList, int userNo) {
@@ -139,9 +98,13 @@ public class HomeService {
 		for(String goodsId : rentList) {
 			// rentVo 객체 생성후 정보 setting
 			List<RentVO> rentBookList = bookMapper.getRentList();
+			
+			logger.debug("대여한 책 리스트 : {}", rentBookList);
+			
+			// 해당 책이 이미 빌려가졌다면
 			for(RentVO rentBook : rentBookList) {
-				if(rentBook.getGoodsId() == goodsId) {
-					break;
+				if(rentBook.getGoodsId().equals(goodsId)) {
+					return null;
 				}
 			}
 			
