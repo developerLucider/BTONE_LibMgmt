ca2c0726defc23e389c62bda6158de55362f6b14
김민석 <kms63123@gmail.com>
2023-04-01 14:30:13 +0900

    jinService 및 fileVo 수정 사항 적용(오류 제거)

--- src/main/java/com/jincomp/jintest/web/jin/service/JinService.java
+++ src/main/java/com/jincomp/jintest/web/jin/service/JinService.java
@@ -25,7 +25,7 @@ import com.jincomp.jintest.web.jin.vo.JinTestVO;
 import com.jincomp.jintest.web.jin.mapper.AdminMapper;
 import com.jincomp.jintest.web.jin.mapper.BookMapper;
 import com.jincomp.jintest.web.jin.vo.BookVO;
-import com.jincomp.jintest.web.jin.vo.fileVO;
+import com.jincomp.jintest.web.jin.vo.FileVO;
 
 import lombok.RequiredArgsConstructor;
 
